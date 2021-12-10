c35fc636845d6c95339d79090549f1ad10ee4c6b
diegodevs <diegom.develops@gmail.com>
2021-12-09 22:15:16 -0400

    [FIX] Virtual type in Order

--- src/db/migrations/20211208163028-order.js
+++ src/db/migrations/20211208163028-order.js
@@ -28,16 +28,6 @@ module.exports = {
         field: 'created_at',
         defaultValue: Sequelize.NOW,
       },
-      total: {
-        type: DataTypes.VIRTUAL,
-        get() {
-          if (this.items.length > 0) {
-            return this.items.reduce((total, item) => {
-              return total + (item.price * item.OrderProduct.amount)
-            }, 0)
-          }
-        }
-      }
     });
   },
 
